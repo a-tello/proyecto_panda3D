14d4b7cb0b83343087c55e6b10688a99a02df3d2
Ale <alee.e.tello@gmail.com>
2025-11-19 16:59:55 -0300

    update GUI visibility

--- interfaz.py
+++ interfaz.py
@@ -7,7 +7,7 @@ from direct.gui.DirectGui import DirectFrame
 
 class Interfaz():
     def __init__(self, juego):
-        #self.fondo = DirectFrame(frameColor = (0, 0, 0, 0), frameSize = (-1, 1, -1, 1), parent = juego.render2d)
+        self.menu = DirectFrame(frameColor = (1, 1, 1, 0))
         self.juego = juego
         self.vida = juego.jugador.vida_max
         self.nivel = juego.nivel
@@ -18,28 +18,48 @@ class Interfaz():
         self.municion_maxima = juego.jugador.municion_maxima
         self.cargador = juego.jugador.cargador
 
-    def crear_GUI(self):
-        self.cargar_iconos_vida()
-        self.puntos_gui = OnscreenText(text = '0', pos = (-1.25, 0.75), mayChange = True, scale=.1, fg=(255,255,255,255), align=TextNode.ALeft)
-        self.objetivo_gui = OnscreenText(text = f'0/{self.vecinos_total}', pos = (1.20, 0.8), mayChange = True, scale=0.1, fg=(255,255,255,255))
-        self.objetivo_img_activo = OnscreenImage(image = 'assets/GUI/Level/Star/Active.png', pos = (1.05, 0, 0.82), scale = 0.09)
-        self.objetivo_img_inactivo = OnscreenImage(image = 'assets/GUI/Level/Star/Unactive.png', pos = (1.05, 0, 0.82), scale = 0.09)
-        self.objetivo_img_activo.setTransparency(True)
-        self.objetivo_img_inactivo.setTransparency(True)
-        self.objetivo_img_activo.hide()
-        self.mira = OnscreenText(text = '+', pos = (0,0,0), mayChange = True, scale=.1, fg=(255,255,255,255), align = TextNode.ALeft)
-        self.balas_gui = OnscreenText(text = f'{self.municion_maxima}/{self.cargador}', pos = (-1.05, -0.8), mayChange = True, scale=0.1, fg=(255,255,255,255))
-        self.balas_img = OnscreenImage(image = 'assets/GUI/Bullet.png', pos = (-1.25, 0, -0.78), scale = (.03, .05, .05))
-        self.balas_img.setTransparency(True)
-
     def actualizar_vida(self, vida):
         self.vida = vida
+        print(len(self.iconos_vida_true))
         for i, icono in enumerate(self.iconos_vida_true):
+            print(i)
             if i < self.vida:
+                print('mostar')
                 icono.show()
                 self.iconos_vida_false[i].hide()
             else:
                 self.iconos_vida_false[i].show()
+    
+    def cargar_iconos_vida(self):
+        for i in range(self.vida):
+            vida_img_true = OnscreenImage(image = 'assets/objects/vida_completa.png',
+                                pos = (-1.25 + i * .06, 0, 0.9), scale=(.04,1,.07), parent = self.menu)
+                                
+            vida_img_false = OnscreenImage(image = 'assets/objects/vida_vacia.png',
+                                pos=(-1.25 + i * .06, 0, 0.9), scale=(.04,1,.07), parent = self.menu)
+            
+            vida_img_true.setTransparency(True)
+            vida_img_false.setTransparency(True)
+            vida_img_true.hide()
+            vida_img_false.hide()
+            self.iconos_vida_true.append(vida_img_true)
+            self.iconos_vida_false.append(vida_img_false)
+    
+    def crear_GUI(self):
+        self.puntos_gui = OnscreenText(text = '0', pos = (-1.25, 0.75), mayChange = True, scale=.1, fg=(255,255,255,255), align=TextNode.ALeft, parent=self.menu)
+        self.objetivo_gui = OnscreenText(text = f'0/{self.vecinos_total}', pos = (1.20, 0.8), mayChange = True, scale=0.1, fg=(255,255,255,255), parent=self.menu)
+        self.objetivo_img_activo = OnscreenImage(image = 'assets/GUI/Level/Star/Active.png', pos = (1.05, 0, 0.82), scale = 0.09, parent=self.menu)
+        self.objetivo_img_inactivo = OnscreenImage(image = 'assets/GUI/Level/Star/Unactive.png', pos = (1.05, 0, 0.82), scale = 0.09, parent=self.menu)
+        self.objetivo_img_activo.setTransparency(True)
+        self.objetivo_img_inactivo.setTransparency(True)
+        self.objetivo_img_activo.hide()
+        self.mira = OnscreenText(text = '+', pos = (0,0,0), mayChange = True, scale=.1, fg=(255,255,255,255), align = TextNode.ALeft, parent=self.menu)
+        self.balas_gui = OnscreenText(text = f'{self.municion_maxima}/{self.cargador}', pos = (-1.05, -0.8), mayChange = True, scale=0.1, fg=(255,255,255,255), parent=self.menu)
+        self.balas_img = OnscreenImage(image = 'assets/GUI/Bullet.png', pos = (-1.25, 0, -0.78), scale = (.03, .05, .05), parent=self.menu)
+        self.balas_img.setTransparency(True)
+        self.cargar_iconos_vida()
+        self.actualizar_vida(self.vida)
+
 
     def actualizar_puntos(self, puntos):
         self.puntos_gui.setText(str(puntos))
@@ -54,58 +74,21 @@ class Interfaz():
     def actualizar_balas(self, balas, cargador):
         self.balas_gui.setText(str(f'{balas}/{cargador}'))
 
-    def cargar_iconos_vida(self):
-        for i in range(self.vida):
-            vida_img_true = OnscreenImage(image = 'assets/objects/vida_completa.png',
-                                pos = (-1.25 + i * .06, 0, 0.9), scale=(.04,1,.07))
-                                
-            vida_img_false = OnscreenImage(image = 'assets/objects/vida_vacia.png',
-                                pos=(-1.25 + i * .06, 0, 0.9), scale=(.04,1,.07))
-            
-            vida_img_true.setTransparency(True)
-            vida_img_false.setTransparency(True)
-            vida_img_true.hide()
-            vida_img_false.hide()
-            self.iconos_vida_true.append(vida_img_true)
-            self.iconos_vida_false.append(vida_img_false)
-    
     def inicializar(self):
         self.objetivo_img_inactivo.show()
         self.objetivo_img_activo.hide()
         self.vida = self.juego.jugador.vida_max
-        self.actualizar_vida(10)
+        self.actualizar_vida(self.vida)
         self.vecinos_salvados = 0
         self.vecinos_total = self.juego.niveles[self.juego.nivel]['vecinos']
         self.objetivo_gui.setText(str(f'{self.vecinos_salvados}/{self.vecinos_total}'))
         
     def esconder(self):
-        #self.cargar_iconos_vida()
-        self.puntos_gui.hide()
-        self.objetivo_gui.hide()
-        self.mira.hide() 
-        self.balas_gui.hide()
-        self.balas_img.hide()
-        
-        if not self.objetivo_img_activo.isHidden():
-            self.objetivo_img_activo.hide()
-        if not self.objetivo_img_inactivo.isHidden():
-            self.objetivo_img_inactivo.hide()
-            
-        for i, icono in enumerate(self.iconos_vida_true):
-                icono.hide()
-                self.iconos_vida_false[i].hide()
-        
+        self.menu.hide()
+
     def mostrar(self):
-        self.puntos_gui.show()
-        self.objetivo_gui.show()
-        self.mira.show() 
-        self.balas_gui.show()
-        self.balas_img.show()
-        
-        if self.vecinos_salvados == self.vecinos_total:
-            self.objetivo_img_activo.show()
-        else:
-            self.objetivo_img_inactivo.show()
+        self.menu.show()
+
         
 
         
